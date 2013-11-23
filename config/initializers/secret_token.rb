d54a330d161900e675a199ac8c5cdee7bb49a0b9
Paolo Cosentino <cosepaol88@gmail.com>
2013-11-23 10:50:58 -0600

    SassyApp First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SassyApp::Application.config.secret_key_base = 'b852a1fc2dd5be12c2379ef4c5c9ff869c81d7702097660690185c3395483b1efb2df04f249c268111ce1bff46428e43969d463be92a4976d214fff41f0571bd'
