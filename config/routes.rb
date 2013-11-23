d1f9e69e8a85a02ae371212da011edcaac7a54e0
Paolo Cosentino <cosepaol88@gmail.com>
2013-11-23 12:03:54 -0600

    Basic sassy setup

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,10 @@ SassyApp::Application.routes.draw do
   #     end
   #   end
   
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'pages#index'
+  
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
